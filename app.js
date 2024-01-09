fcd92ff220ecf5d208d6f36175c37ce9e51ff95a
Sahil Sukhdeve <121704509+Sahil-Sukhdeve12@users.noreply.github.com>
2024-01-10 00:34:11 +0530

    1

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use("/listings",listingRouter);
+app.use("/",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
@@ -98,7 +98,7 @@ app.all("*",(req,res,next)=>{
 
 app.use((err,req,res,next)=>{
     let{statusCode=500,message="something went wrong!!"}=err;
-    res.status(statusCode).render("error.ejs",{message});
+    res.status(statusCode).render("listings/error.ejs",{message});
 });
 
 app.listen(8000,()=>{
